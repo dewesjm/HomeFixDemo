987f75aec2e4d5d1f1dcaa5029e52d353d27d5ad
dewes <dewesjm@gmail.com>
2026-09-12 11:38:57 -0400

    NDT overhaul: probationary checkbox, partial, procedure used, penetrant split, RT fields, ID accessible, version bump for NQC

--- src/app/job-detail/job-detail.component.ts
+++ src/app/job-detail/job-detail.component.ts
@@ -107,7 +107,7 @@ export class JobDetailComponent {
   isNdtStage = computed(() => {
     if (!this.wf) return false;
     const stage = this.wf().stages[this.selectedStep()];
-    return stage?.id?.startsWith('root-ndt') || stage?.id?.startsWith('final-ndt');
+    return stage?.id?.startsWith('root-ndt') || stage?.id?.startsWith('layer-ndt') || stage?.id?.startsWith('final-ndt');
   });
 
 //extra fields when you press show more
