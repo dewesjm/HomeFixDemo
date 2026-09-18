8abf698e4bbb78bf989c462ee45f4431befd4582
dewes <dewesjm@gmail.com>
2026-09-18 18:28:08 -0400

    Clear seeded W.E. Memo value

--- src/app/data/workflow.ts
+++ src/app/data/workflow.ts
@@ -1192,7 +1192,7 @@ export function newWorkflow(job: Job): JobWorkflow {
     id2: 'MIC-4411',
     drawingRev: 'C',
     actualThickness: '0.75',
-    weldMemo: 'Standard weld procedure',
+    weldMemo: '',
     revisedJointDesign: 'bj-g',
     changeNumber: 'ER-0042',
     wtn: Math.random() < 0.5 ? 'wtn-101' : 'wtn-201',
