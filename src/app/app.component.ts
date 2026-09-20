cbf61d66fe1be4251264b8552bd21c772129cf6c
dewes <dewesjm@gmail.com>
2026-09-20 16:28:51 -0400

    feat: simplify Weld Engineering menu - Procedure Lookup, Admin with Manage/Load Procedures

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,7 @@ import {
   LucideTable, LucideHistory, LucideSlidersHorizontal, LucideSettings, LucideWorkflow, LucideTag,
   LucideStepForward, LucideCircleArrowUp, LucideRefreshCw,
   LucideBadgeCheck, LucideMapPin, LucideTarget, LucideMegaphone, LucideClipboardList, LucideLayers,
-    LucideShield, LucideLink, LucideUpload
+    LucideShield, LucideLink, LucideUpload, LucideSearch
 } from '@lucide/angular';
 
 // check for version updates periodically, only full refresh will check
@@ -27,7 +27,7 @@ const UPDATE_POLL_MS = 5 * 60 * 1000;
     LucideCircleArrowUp, LucideRefreshCw, LucideBadgeCheck, LucideMapPin, LucideTarget,
     LucideTable, LucideHistory, LucideSlidersHorizontal, LucideSettings, LucideWorkflow, LucideTag,
   LucideStepForward, LucideMegaphone, LucideClipboardList, LucideLayers,
-  LucideShield, LucideLink, LucideUpload
+  LucideShield, LucideLink, LucideUpload, LucideSearch
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
